d1d499151f22d629e1521c85121f757a7cded558
princess <princessmumbi001@gmail.com>
2024-07-17 13:18:22 +0300

    models update

--- server/models.py
+++ server/models.py
@@ -10,23 +10,53 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
-
-class Customer(db.Model):
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
+
+    
+    reviews = db.relationship('Review', back_populates='customer')
+
+    
+    items = association_proxy('reviews', 'item')
+
+    
+    serialize_rules = ('-reviews.customer',)
 
     def __repr__(self):
-        return f'<Customer {self.id}, {self.name}>'
+        return f'<Customer id={self.id} name={self.name}>'
 
-
-class Item(db.Model):
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    price = db.Column(db.Float)
+    name = db.Column(db.String, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+    
+    reviews = db.relationship('Review', back_populates='item')
+
+    serialize_rules = ('-reviews.item',)
 
     def __repr__(self):
-        return f'<Item {self.id}, {self.name}, {self.price}>'
+        return f'<Item id={self.id} name={self.name} price={self.price}>'
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String, nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'), nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'), nullable=False)
+
+   
+    customer = db.relationship('Customer', back_populates='reviews')
+    item = db.relationship('Item', back_populates='reviews')
+
+   
+    serialize_rules = ('-customer.reviews', '-item.reviews')
+
+    def __repr__(self):
+        return f'<Review id={self.id} comment={self.comment} customer_id={self.customer_id} item_id={self.item_id}>'
